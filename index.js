b2a48490a1d290f15489eb6e2907212fcb954055
DamianoSchirinzi-Dev <DamianoSchirinzi.Dev@gmail.com>
2022-11-29 20:56:55 +0000

    Implementing site scraper for different news sites.

--- index.js
+++ index.js
@@ -1,78 +1,45 @@
 const Joi = require("joi");
+const axios = require("axios");
+const cheerio = require("cheerio");
 const express = require("express");
 const app = express();
 
 app.use(express.json());
 
-const people = [
-  { name: "John", id: 1 },
-  { name: "Sarah", id: 2 },
-  { name: "Natalie", id: 3 },
-  { name: "Damiano", id: 4 },
-  { name: "Taco", id: 5 },
-];
+//The Gaurdian
+app.get("/api/scrape/gaurdian", async (req, res) => {
+  axios
+    .get(req.query.url)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
 
-const personSchema = Joi.object({
-  name: Joi.string().min(3).required(),
+      const scrapedData = {
+        "article-title": $(
+          "[data-gu-name|='headline'] > div > div > h1"
+        ).text(),
+
+        "article-sub-heading": $(
+          "[data-gu-name|='standfirst'] > div > p"
+        ).text(),
+      };
+
+      $("#maincontent > div", html).each(function () {
+        const content = $(this).text().replaceAll(".", ". ");
+
+        scrapedData["article-summary"] = content;
+      });
+
+      res.send(scrapedData);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
-
-// #region Endpoints
-//Get all people
-app.get("/api/people", (req, res) => {
-  res.send(people);
-});
-
-//Get person by ID
-app.get("/api/people/:id", (req, res) => {
-  const person = findPersonById(req);
-  if (!person)
-    res
-      .status(404)
-      .send("Error 404: The person with the given ID was not found");
-
-  res.send(person);
-});
-
-//Add new person
-app.post("/api/people", (req, res) => {
-  validateSchema(personSchema, req, res);
-
-  const newPerson = {
-    id: people.length + 1,
-    name: req.body.name,
-  };
-
-  people.push(newPerson);
-  res.send(newPerson);
-});
-
-//Update existing person
-app.put("/api/people/:id", (req, res) => {
-  const person = findPersonById(req);
-  if (!person)
-    return res
-      .status(404)
-      .send("Error 404: The person with the given ID was not found");
-
-  validateSchema(personSchema, req, res);
-
-  person.name = req.body.name;
-  res.send(person);
-});
-// #endregion
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Live on http://localhost:${port}...`));
 
-// #region Functions
-function validateSchema(schema, req, res) {
-  result = schema.validate(req.body);
-
-  if (result.error) return res.status(400).send(result.error.message);
-  else return result;
+function replaceAll(str, find, replace) {
+  return str.replace(new RegExp(find, "g"), replace);
 }
-
-function findPersonById(req) {
-  return people.find((p) => p.id === parseInt(req.params.id));
-}
-//#endregion
